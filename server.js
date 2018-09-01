12b1216cc0d2ba6fe371d5f08e5c4c6a139df046
Clovis OLIVIER <clovis.olivier@gmail.com>
2018-09-01 21:34:57 +0200

    add swagger documentation

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    swaggerpath = express(),
     app = express(),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
@@ -6,10 +7,10 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     User = require('./api/models/userModel'),
     jsonwebtoken = require("jsonwebtoken"),
-    config = require('./config/index');
+    config = require('./config/index'),
+    argv = require('minimist')(process.argv.slice(2));
 
 mongoose.Promise = global.Promise;
-console.log('config.db :'+config.db);
 mongoose.connect(config.db, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -31,10 +32,41 @@ app.use(function(req, res, next){
 var routes = require('./api/routes/todoListRoutes');
 routes(app);
 
+app.use("/v1", swaggerpath);
+var swagger = require('swagger-node-express').createNew(swaggerpath);
+app.use(express.static('dist'));
+swagger.setApiInfo({
+    title: "Auth API doc",
+    description: "API to manage user authentification",
+    termsOfServiceUrl: "",
+    contact: "clovis.olivier@gmail.com",
+    license: "MIT",
+    licenseUrl: "https://opensource.org/licenses/MIT"
+});
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
 app.use(function(req, res){
     res.status(404).send({url: req.originalUrl +' not found'})
 });
 
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+	// Configure the API domain
+	var domain = 'localhost';
+	if(argv.domain !== undefined)
+	    domain = argv.domain;
+	else
+	    console.log('No --domain=xxx specified, taking default hostname "localhost".')
+
+	
+	// Set and display the application URL
+	var applicationUrl = 'http://' + domain + ':' + port;
+	console.log('snapJob API running on ' + applicationUrl);
+
+
+    swagger.configure(applicationUrl, '1.0.0');
+    
 app.listen(port);
 
 console.log('todo list Restful AP server started on : ' +port);
